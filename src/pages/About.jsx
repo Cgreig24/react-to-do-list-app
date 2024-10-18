8a8742bdee6eefb73db523f736abb89512af3089
Coco Greig <cocogreig@hotmail.co.uk>
2024-10-18 10:20:47 +0200

    added github repo

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,12 @@ function About() {
       <h1 className="aboutHeader">About this Website</h1>
       <p>This is a to do website to help you get through your to do list</p>
       <p>It was created by Coco Greig as an Ironhack mini project</p>
-      <p>GitHub repo can be found here</p>
+      <p>
+        GitHub repo can be found{" "}
+        <span>
+          <a href="https://github.com/Cgreig24/react-to-do-list-app"> here</a>
+        </span>
+      </p>
     </div>
   );
 }
